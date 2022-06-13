3607cfd461d1e925d735898fb706f9be17a46bd7
imjord <dean.jay.goat@gmail.com>
2022-06-12 23:26:07 -0600

    need to fix database to add owners and members:

--- client/src/Components/Club.js
+++ client/src/Components/Club.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 const Club = (props) => {
@@ -10,11 +11,22 @@ const Club = (props) => {
         getClub(id);    // getClub is a function from App.js
     }, [])
 
-    const { name, description} = club;
+  
 
   return (
     loading ? <h1>Loading...</h1> : <div>
-    {club.map(club => <div key={club.club_id}>{club.name}</div>)}
+      {/* link back to /clubs */}
+      <Link to="/clubs">Back to Clubs</Link>
+    {club.map(club => <div key={club.club_id}>
+    <h3>description</h3>
+    <p>{club.description}</p>
+    <p>Club member Count: NA</p>
+    <h3>Club Members</h3>
+    <ul>
+      <li> need to implement backend </li>
+    </ul>
+    <p>club owner: </p>
+    </div>)}
        
     </div>
   )
